ab2c8c252fdf182cee1063055953a2803ed4b861
yopperpo <yopperpo@gmail.com>
2018-05-07 22:17:28 -0700

    added Distance, Time, and Direction classes

--- mapquest_api.py
+++ mapquest_api.py
@@ -11,6 +11,8 @@ import urllib.request
 
 
 MAPQUEST_API_KEY = 'LavK4KKvuP4NEHf7h8VWTq8oGLakIFmA'
+base_mapquest_url = "http://open.mapquestapi.com/directions/v2"
+
 
 
 def get_locations(number_of_locations:int)->list:
@@ -24,6 +26,19 @@ def get_locations(number_of_locations:int)->list:
             locations.append(location)
         return locations
 
+
+def locations_to_tuples(locations:list)-> list:
+    'Given a list of locations, returns a list of tuples with the locations in query format'
+    location_tuples = []
+    for location in locations:
+        if location == locations[0]:
+            first_location = ("from",location)
+            location_tuples.append(first_location)
+        else:
+            remaining_locations = ("to", location)
+            location_tuples.append(remaining_locations)
+    return location_tuples
+
 def get_outputs(number_of_outputs:int)->list:
     'Given a number of outputs, return a list containing which outputs to use'
     outputs = []
@@ -36,8 +51,25 @@ def get_outputs(number_of_outputs:int)->list:
         return outputs
 
 
-def build_url()->str:
-    'Takes your base URL, adds the necessary parameters to make an API Request to MapQuest'
-    query_parameters = [('key',MAPQUEST_API_KEY), ]
 
+def build_url(list_of_locations:list)->str:
+    'Takes your base URL, adds the necessary parameters to make an API Request to MapQuest'
+    key_parameter = [('key',MAPQUEST_API_KEY)]
+    locations_to_use = locations_to_tuples(list_of_locations)
+    query_parameters = key_parameter+locations_to_use
+
+    return base_mapquest_url + '/route?' + urllib.parse.urlencode(query_parameters)
+
+def parse_url(page_url:str)-> dict:
+    'Takes a url and returns a python dictionary representing the parsed JSON response'
+    response = None
+
+    try:
+        response = urllib.request.urlopen(page_url)
+        json_text = response.read().decode(encoding = 'utf-8')
+        return json.loads(json_text)
+
+    finally:
+        if response != None:
+            response.close()
 
--- user_interface.py
+++ user_interface.py
@@ -2,7 +2,7 @@
 
 # A module that reads the input and constructs the objects that will generate the program's output. '
 #'This is the only module that should have an if __name__ == '__main__' block to make it executable;
-#  you would execute this module to run your program.
+# you would execute this module to run your program.
 
 import mapquest_api
 
@@ -10,12 +10,16 @@ import mapquest_api
 def user_interface() -> None:
     'Initializes the main program and takes in user input'
     num_of_locations = int(input())
-    mapquest_api.get_locations(num_of_locations)
+    locations = mapquest_api.get_locations(num_of_locations)
     num_of_outputs = int(input())
-    mapquest_api.get_outputs(num_of_outputs)
+    outputs = mapquest_api.get_outputs(num_of_outputs)
+
+    print(mapquest_api.build_url(locations))
+    mapquest_data = mapquest_api.parse_url(mapquest_api.build_url(locations))
 
 
 
 
 if __name__ == '__main__':
     user_interface()
+
